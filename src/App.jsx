3017ed5e61edbd5129a074d5cd6ad94d0cd5d8dc
Consue <c.anguitasalinas@gmail.com>
2022-09-02 23:48:32 -0400

    feat: new component order

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,13 @@
 import './App.css';
 import List from './components/list-of-collaborators/List';
-import Navbar from './layout/navbar/Navbar';
-import FormCollaborator from './components/form-collaboratos/Form';
-import { useState } from 'react';
 import data from './assets/data/data.json';
 
 function App() {
-  const [collaborators, setCollaborator] = useState(data);
-  const [collabList, setCollabList] = useState(collaborators);
+  const dataInfo= data;
 
   return (
     <>
-      <Navbar  data={collaborators} setCollabList={setCollabList}/>
-      <FormCollaborator 
-        data={collaborators} 
-        setCollaborator={setCollaborator} 
-      />
-      <List collabList={collabList}/> 
+      <List data={dataInfo}/> 
     </>
   )
 }
--- src/components/list-of-collaborators/List.jsx
+++ src/components/list-of-collaborators/List.jsx
@@ -1,9 +1,109 @@
-export default function List({collabList}) {
+import { useState } from "react";
+
+export default function List({data}) {
   
+  const [collaborators, setCollaborator] = useState(data);
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [search, setSearch] = useState("");
+
+  const handdler = (e) => {
+    e.preventDefault();
+    const idMapping = Number(collaborators[collaborators.length - 1].id) + 1;
+    let userAdded = {
+      "id": idMapping.toString(),
+      "nombre": name,
+      "correo": email
+    }
+    setCollaborator([...collaborators, userAdded]);
+    setName("");
+    setEmail("");
+  }
+
+  const searchHandler = (e) => {
+    setSearch(e.target.value);
+  }
+
+  const list = !search 
+    ? collaborators 
+    : collaborators.filter((x) => x.nombre.toLowerCase().includes(search.toLocaleLowerCase())
+    );
+
   return (
     <>
+    <header className="bg-gray-50">
+      <div className="flex items-center px-4 py-8 mx-auto max-w-screen-xl sm:px-6 lg:px-8">
+        <div>
+          <h1 className="text-2xl font-bold text-gray-900 sm:text-3xl ">
+            Buscador de colaboradores
+          </h1>
+        </div>
+        <div className="relative">
+          <label className="sr-only"> Search </label>
+          <input
+            className="w-full h-10 pl-4 pr-10 text-sm bg-white border-none rounded-full shadow-sm sm:w-56"
+            id="search"
+            type="search"
+            placeholder="Buscar un colaborador"
+            value={search}
+            onChange={(e) => searchHandler(e)}
+          />
+          {/* <button
+            className="absolute p-2 text-gray-600 rounded-full transition -translate-y-1/2 hover:text-gray-700 bg-gray-50 top-1/2 right-1"
+            type="button"
+            aria-label="Submit Search"
+            onClick={searchHandler}
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="w-4 h-4"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              strokeWidth="2"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+              />
+            </svg>
+          </button> */}
+        </div>
+      </div>
+    </header>
+
+    <form onSubmit={handdler}>
+      <label className="relative block p-3 border-2 border-gray-200 rounded-lg">
+        <span className="text-xs font-medium text-gray-500">
+          Nombre
+        </span>
+        <input className="w-full p-0 text-sm border-none focus:ring-0" 
+          id="nombre" 
+          type="text" 
+          placeholder="Ingresa el nombre del colaborador"
+          value={name} 
+          onChange={(e) => setName(e.target.value)} 
+        />
+      </label>
+        <br />
+      <label className="relative block p-3 border-2 border-gray-200 rounded-lg" >
+        <span className="text-xs font-medium text-gray-500" >
+          Correo
+        </span>
+        <input className="w-full p-0 text-sm border-none focus:ring-0" 
+          id="correo" 
+          type="email" 
+          placeholder="Ingresa correo del colaborador"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        </label>
+      <button>Agregar colaborador</button>
+    </form>
+
     <ul>
-      {collabList.map((collaborator) => (
+      {list.map((collaborator) => (
         <li key={collaborator.id}>{collaborator.nombre} {collaborator.correo}
         </li>
       ))}
